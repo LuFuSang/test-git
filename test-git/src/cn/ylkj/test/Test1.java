2beeb6badc083e98f78bfa4486df4215dc7ad56e
LuFuSang <1064931698@qq.com>
2018-12-10 11:07:13 +0800

    修改了分支内容

--- test-git/src/cn/ylkj/test/Test1.java
+++ test-git/src/cn/ylkj/test/Test1.java
@@ -2,6 +2,11 @@ package cn.ylkj.test;
 
 public class Test1 {
 	public static void main(String[] args) {
+		int i = 0;
 		System.out.println(i);
+	}
+	
+	public void test() {
+		System.out.println("test");
 	}
 }
