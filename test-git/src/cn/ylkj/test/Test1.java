f685bf7dc350351473a16e69aa0d597efc2e8a52
LuFuSang <1064931698@qq.com>
2018-12-10 11:15:50 +0800

    再一次测试冲突

--- test-git/src/cn/ylkj/test/Test1.java
+++ test-git/src/cn/ylkj/test/Test1.java
@@ -2,9 +2,9 @@ package cn.ylkj.test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		int i = 1;
+		System.out.println();
 		int b = 2;
-		System.out.println(i);
+		System.out.println(b);
 	}
 	
 	public void test() {
